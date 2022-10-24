b2e55c8355d9c6c75b156cc58075e28b2fe2a36e
Damian Sebastian Silvera <116028887+Damiansebastians@users.noreply.github.com>
2022-10-24 15:19:32 +0200

    Add files via upload

---
+++ Archivo.js
@@ -0,0 +1,26 @@
+
+var Numero1=25;
+var Numero2=15;
+var Numero3;
+var Nombre="Juan";
+var Apellido1="Perez";
+var Apellido2="Gonzalez";
+
+//document.write("La variable Nº1 contiene:" + Numero1);
+//document.write("<br>La variable Nº1 contiene:" + Numero2);
+//document.write(Numero4);
+//document.write(Numero3);
+
+document.write("<br>Mi nombre es: " + Nombre +" "+ Apellido1 +" "+ Apellido2);
+
+document.write("<br><br>Nombre: " + Nombre);
+document.write("<br>Primer Apellido: " + Apellido1);
+document.write("<br>Segundo Apellido: " + Apellido2);
+
+var Valor1=5;
+var Valor2=10;
+var Resultado=Valor1+Valor2;
+var ResultadoResta=Valor1-Valor2;
+
+document.write("<br><br> La suma de " + Valor1 +" + " + Valor2 + " es igual a " + Resultado);
+document.write("<br> La resta es igual a " + ResultadoResta);
